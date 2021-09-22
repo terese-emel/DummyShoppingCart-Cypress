2da176a6164c4ca99479052a427b8c7425196a2c
terese.emel@gmail.com <terese.emel@gmail.com>
2021-09-22 15:49:23 +0200

    Add items to cart

---
+++ cypress/integration/Add items to the cart copy/addItemsToCart.spec.js
@@ -0,0 +1,45 @@
+
+describe("Add items to the cart and verify total items in the cart", () => {
+  const isNumeric = (cell) => Cypress._.isFinite(+cell.textContent);
+  const filterNumeric = (cells$) => Cypress._.filter(cells$, isNumeric);
+  const toStrings = (cells$) => Cypress._.map(cells$, "textContent");
+  const toNumbers = (texts) => Cypress._.map(texts, Number);
+  const sum = (numbers) => Cypress._.sum(numbers);
+  const sumAgeValues = Cypress._.flow([toStrings, toNumbers, sum]);
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("http://localhost:3001");
+  });
+
+
+  it("Add at least one from all available items to the cart", () => {
+    cy.get(".row").find(".btn-secondary").click({ multiple: true });
+
+    cy.get(".row")
+      .find(".col-md-1")
+      .find(".badge-primary")
+      .then(filterNumeric)
+      .then(sumAgeValues)
+      .then((cellsTotal) => {
+        cy.get(".badge-pill").then(sumAgeValues).should("eq", cellsTotal);
+      });
+  });
+
+ it("Add multiple items multiple times to the cart and verify total count", () => {
+   for (let i = 0; i<5;i++) {
+     cy.get(".row").find(".btn-secondary").click({ multiple: true });
+   }
+
+   cy.get(".row")
+     .find(".col-md-1")
+     .find(".badge-warning")
+     .then(filterNumeric)
+     .then(sumAgeValues)
+     .then((cellsTotal) => {
+       cy.get(".badge-pill").then(sumAgeValues).should("eq", cellsTotal);
+     });
+ });
+});
